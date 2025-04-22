1f322ab02c7c34e20172d1b9748abf83e343e5f3
Elitearcanist <archtyepeofepic@gmail.com>
2025-04-22 13:00:28 -0600

    project file changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
+import sphinx_pdj_theme
 
 # -- Project information
 
@@ -29,7 +30,6 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-import sphinx_pdj_theme
 html_theme = 'sphinx_pdj_theme'
 htm_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
